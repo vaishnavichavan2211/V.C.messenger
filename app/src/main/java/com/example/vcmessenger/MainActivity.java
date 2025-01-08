a61fa017291118ed8ecfe4c45a25a794fbecca26
karan <karan.suthar.lakha@gmail.com>
2025-01-08 13:57:31 +0530

    code format

--- app/src/main/java/com/example/vcmessenger/MainActivity.java
+++ app/src/main/java/com/example/vcmessenger/MainActivity.java
@@ -3,28 +3,19 @@ package com.example.vcmessenger;
 import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
-import android.media.MediaSession2Service;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -38,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
    ArrayList<Users> usersArrayList;
    ImageView imglogout;
 
-    ImageView cumbut,setbut;
+    ImageView cumbut, setbut;
 
     @SuppressLint({"MissingInflatedId"})
     @Override
@@ -48,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        database=FirebaseDatabase.getInstance();
+        database = FirebaseDatabase.getInstance();
         auth = FirebaseAuth.getInstance();
         cumbut = findViewById(R.id.camBut);
         setbut = findViewById(R.id.settingBut);
@@ -75,16 +66,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        imglogout =findViewById(R.id.logoutimg);
+        imglogout = findViewById(R.id.logoutimg);
         imglogout.setOnClickListener(v -> {
-            Dialog dialog=new Dialog(MainActivity.this,R.style.dialoge);
+            Dialog dialog = new Dialog(MainActivity.this, R.style.dialoge);
             dialog.setContentView(R.layout.dialog_layout);
-            Button no,yes;
-            yes=dialog.findViewById(R.id.yesbnt);
-            no=dialog.findViewById(R.id.nobnt);
+            Button no, yes;
+            yes = dialog.findViewById(R.id.yesbnt);
+            no = dialog.findViewById(R.id.nobnt);
             yes.setOnClickListener(v2 -> {
                 FirebaseAuth.getInstance().signOut();
-                Intent intent=new Intent(MainActivity.this,login.class);
+                Intent intent = new Intent(MainActivity.this, login.class);
                 startActivity(intent);
                 finish();
             });
@@ -99,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
         cumbut.setOnClickListener(v -> {
             Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(intent,10);
+            startActivityForResult(intent, 10);
         });
     }
 }
